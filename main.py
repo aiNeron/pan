d31a497ee42baae4d569be79589030572ddadf22
pan <neronlopez7@gmail.com>
2024-10-17 20:57:32 +0000

    PAN 2 (use variables y condiciones)

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-name="luca y neron"
-print(name)
+name="neron"
+numero = 3
+booleano: True
+booleano: False
+print(numero)
+print(3)
+neronedad = 16
+edadparavotar = 16
+puedevotar = edadparavotar <= neronedad
+
+if puedevotar:
+    print('puede votar')
